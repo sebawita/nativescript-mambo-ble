52e35c46a11371ac79f8496912ff5a062c5e37a6
Sebastian Witalec <sebawita@gmail.com>
2017-03-30 13:34:15 +0100

    Added check to return Mambo devices only

--- drone-scanner.d.ts
+++ drone-scanner.d.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { Drone } from './drone';
 export declare class DroneScanner {
     static scan(): Observable<Drone>;
+    private static checkIfMambo(peripheral);
     static initialisePermissionsIfRequired(): void;
     static hasPermissions(): Promise<boolean>;
     static requestPermissions(): void;
--- drone-scanner.ts
+++ drone-scanner.ts
@@ -1,15 +1,12 @@
-import { startScanning, requestCoarseLocationPermission, hasCoarseLocationPermission, Peripheral } from 'nativescript-bluetooth';
+import { startScanning, requestCoarseLocationPermission, hasCoarseLocationPermission, Peripheral, setCharacteristicLogging } from 'nativescript-bluetooth';
 import { Observable, Subject, Subscription } from 'rxjs';
 
 import { Drone } from './drone';
 
 export class DroneScanner {
 
-  // constructor() {
-  //   this.initialisePermissionsIfRequired();
-  // }
-
   static scan(): Observable<Drone> {
+    setCharacteristicLogging(false);
     this.initialisePermissionsIfRequired();
 
     const subject = new Subject<Drone>();
@@ -18,20 +15,18 @@ export class DroneScanner {
       serviceUUIDs: [],
       seconds: 3,
       onDiscovered: (peripheral: Peripheral) => {
-      // if (this.checkIfMambo(peripheral) === false) {
-        // console.log(`Skipping a non mambo device with UUID: ${peripheral.UUID} and advertisement: ${peripheral.advertisement}`);
-        // return;
-      // }
 
         console.log("");
         console.log("----New Device Found----");
         console.log("Periperhal found with UUID: " + peripheral.UUID);
         console.log("Periperhal found with name: " + peripheral.name);
-        // console.log("Periperhal found with advertisement: " + peripheral.advertisement);
 
-        // var newDevice = new DroneController(peripheral.UUID, peripheral.name, peripheral.state);
+        if (this.checkIfMambo(peripheral) === false) {
+          console.log(`Skipping a non mambo device with UUID: ${peripheral.UUID}`);
+          return;
+        }
+        
         var newDevice = new Drone(peripheral.UUID, peripheral.name);
-
         subject.next(newDevice);
       }
     })
@@ -41,24 +36,16 @@ export class DroneScanner {
 
     return subject;
   }
-/*
-  static checkIfMambo(periperhal: any) {
-    const advert = base64_decode(periperhal.advertisement);
 
-    // check if a WowWee device
-    //  Wowwee has a code 0x03FO, they are listed as CLINK at https://www.bluetooth.com/specifications/assigned-numbers/company-identifiers
-    //  the code should be stored as advert[4] => O3 and advert[5] => F0
+  private static checkIfMambo(peripheral: Peripheral) {
+    if (peripheral.name === null) {
+      return false;
+    }
 
-    // if (advert[4] !== 0x03 || advert[5] !== 0xF0) {
-            //     // this is not a Wowwee device
-    //     // return false;
-    //     console.log("Is this a WowWee device?: " + advert.toString());
-    // }
-
-    // check if mambo, all mambo have the 4th digit set to 5
-    return (advert.length > 3 && advert[3] === 5);
+    const namePrefix = peripheral.name.substr(0, 5);
+    return namePrefix === 'Mambo';
   }
-*/
+
   static initialisePermissionsIfRequired() {
     this.hasPermissions()
     .then(granted => {
@@ -80,40 +67,4 @@ export class DroneScanner {
       }
     );
   }
-}
-
-const keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
-
-function base64_decode(input: string): Array<number> {
-const orig_input = input;
-  input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-  if (orig_input != input)
-    console.error("Warning! Characters outside Base64 range in input string ignored.");
-  if (input.length % 4) {
-    console.error("Error: Input length is not a multiple of 4 bytes.");
-    return [];
-  }
-
-  const output: Array<number> = [];
-
-  let i=0;
-  while (i < input.length) {
-    const enc1 = keyStr.indexOf(input.charAt(i++));
-    const enc2 = keyStr.indexOf(input.charAt(i++));
-    const enc3 = keyStr.indexOf(input.charAt(i++));
-    const enc4 = keyStr.indexOf(input.charAt(i++));
-
-    const chr1 = (enc1 << 2) | (enc2 >> 4);
-    const chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-    const chr3 = ((enc3 & 3) << 6) | enc4;
-    
-    output.push(chr1);
-    if (enc3 != 64) {
-      output.push(chr2);
-    }
-    if (enc4 != 64) {
-      output.push(chr3);
-    };
-  }
-  return output;
 }
